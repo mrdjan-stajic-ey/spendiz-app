988417462ff13da8174eba69d4af2a01596f847d
mrdjan-stajic-ey <mrdjan.stajic@rs.ey.com>
2021-10-27 17:22:39 +0200

    overview screen mock

--- src/pages/account-settings/Overview.tsx
+++ src/pages/account-settings/Overview.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Center} from 'native-base';
+import React, {useContext} from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import AppButton from '../../components/button/AppButton';
 import AppPage from '../../components/page/AppPage';
 import AppText from '../../components/Text/AppText';
+import PhrasesContext from '../../data-management/PhraseContext';
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   content: {
+    flex: 1,
+  },
+  balanceType: {
     flex: 1,
   },
 });
 
 const OverviewPage: React.FC<{}> = (): JSX.Element => {
+  const {phrases, categories} = useContext(PhrasesContext);
   return (
     <AppPage>
-      <View style={style.content}>
+      <View style={styles.content}>
         <AppText type="TITLE" text={'Overview page'} />
+        <AppText
+          text={
+            'You have chosen the following phrases to manage your balance amount'
+          }
+        />
+        <View style={{flex: 1}}>
+          <ScrollView>
+            <View style={styles.balanceType}>
+              {phrases.map(p => {
+                return <Text> {p.text} </Text>;
+              })}
+            </View>
+          </ScrollView>
+        </View>
+        <AppText text={'And they are asociated with categories:'} />
+        <View style={{flex: 1}}>
+          <ScrollView>
+            <View style={styles.balanceType}>
+              {categories.map(p => {
+                return <Text> {p} </Text>;
+              })}
+            </View>
+          </ScrollView>
+        </View>
+        <AppButton text={'Finish'} />
       </View>
     </AppPage>
   );
