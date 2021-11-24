cc6eb6a8eeac1562ca27fe253a31205c74c7044f
mrdjan-stajic-ey <mrdjan.stajic@rs.ey.com>
2021-11-24 15:55:27 +0100

    sms handler sending events

--- src/Sms_Handler.js
+++ src/Sms_Handler.js
@@ -1,12 +1,23 @@
-module.exports = async taskData => {
-  console.log('Sms received', taskData);
+const {
+  default: AsyncStorage,
+} = require('@react-native-async-storage/async-storage');
+const {default: axios} = require('axios');
 
-  // axios
-  //   .get(`http://10.0.2.2:${8080}/api/v1/expenses`)
-  //   .then(res => {
-  //     console.log('Expsenses req', res);
-  //   })
-  //   .catch(err => {
-  //     console.log('error expenseds req', err);
-  //   });
+module.exports = async taskData => {
+  const token = await AsyncStorage.getItem('JWT');
+  axios
+    .post('http://10.0.2.2:3000/sms', taskData, {
+      //TODO: make prebuild script that will change the base url
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }) //TODO: there should be some fallback to this, app should check for timestamps in sms-es db vs client i am not sure this will work
+    //there is a problem with native also;
+    .then(res => {
+      //   console.log('Sms sent', res);
+    })
+    .catch(err => {
+      //   console.log('Sms error', err);
+      return err;
+    });
 };
